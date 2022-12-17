aaf1c72877b2759f85d3c79fc5fb4fdca5562752
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2022-12-17 16:24:01 +0300

    Изучение Шаблонных строк

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-/*
-Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
-*/
+// Шаблонные строки
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
 
-const payRateUSD = 80; /* Ваша часовая ставка 80$ */
-const workSchedule = 5; /* вы готовы работать не более 5 часов в день */
-const workingDays = 5; /* вы готовы работать 5 дней в неделю */
-const openingHours = 40; /* заказ на 40 часов работы */
-const daysToWork = 11; /* Вы должны уехать через 11 дней */
-const finalСost = payRateUSD * openingHours; /* Конечная стоимость работы */
+// Вариант 1 без шаблонной строки
+const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
+console.log(template);
 
+// Вариант 1 с шаблонной строкой
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template2);
 
-const daysNeeded = openingHours / workSchedule;
-const haveDaysForWork = daysToWork - 2;
-let takeAnOrder = haveDaysForWork >= daysNeeded; /* взяться за работу или нет */
+// Вариант 2 без шаблонной строки
+const template3 = 'Проект \n' + 'Цена: ' + price + '$';
+console.log(template3);
 
-console.log('Нужно ' + daysNeeded + ' дней для выполнения заказа');
-console.log('Успею поработать ' + haveDaysForWork + ' дней');
-console.log('Брать заказ? - ' + takeAnOrder);
-console.log('Работа будет стоить ' + finalСost + '$');
+// Вариант 2 с шаблонной строкой
+const template4 = `Проект
+Цена ${price}$`;
+console.log(template4);
