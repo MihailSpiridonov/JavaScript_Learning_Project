131f44601a699de53fbd5c63b87a5b7d7f22097b
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2022-12-17 15:25:10 +0300

    Выполнил первую задачу интенсива и добавил стилей картинке

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello World !");
+/*
+Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
+Выведете в консоль:
+- Boolean переменную успеете ли вы взяться за работу
+- Сколько вы за неё попросите?
+*/
+
+const payRateUSD = 80; /* Ваша часовая ставка 80$ */
+const workSchedule = 5; /* вы готовы работать не более 5 часов в день */
+const workingDays = 5; /* вы готовы работать 5 дней в неделю */
+const openingHours = 40; /* заказ на 40 часов работы */
+const daysToWork = 11; /* Вы должны уехать через 11 дней */
+const finalСost = payRateUSD * openingHours; /* Конечная стоимость работы */
+
+
+const daysNeeded = openingHours / workSchedule;
+const haveDaysForWork = daysToWork - 2;
+let takeAnOrder = haveDaysForWork >= daysNeeded; /* взяться за работу или нет */
+
+console.log('Нужно ' + daysNeeded + ' дней для выполнения заказа');
+console.log('Успею поработать ' + haveDaysForWork + ' дней');
+console.log('Брать заказ? - ' + takeAnOrder);
+console.log('Работа будет стоить ' + finalСost + '$');
