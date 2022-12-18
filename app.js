509457b1556dcb0b8cce9aba0d7ef5017b49b01c
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2022-12-18 10:49:35 +0300

    Изучение switch, и сравнение его  с else if

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-/*
-Васи положил 12 000$ на вклад 7% годовых с капитализацией 1 раз в месяц. Вывести в консоль, сможет ли он купить дом за 13 500$ через 2 года после снятия вклада. И остаток после покупки.
-Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
-*/
+// Изучение switch, else if
 
-const depositUSD = 12000;
-const annualRatePercent = 0.07;
-const depositTerm = 24;
-const homeValue = 13500;
+const role = 'manager';
 
-const monthlyRate = annualRatePercent / 12;
-const accumulatedAmount = depositUSD * (1 + monthlyRate) ** depositTerm;
-const cashResidue = accumulatedAmount - homeValue;
-const NotEnoughMoney = homeValue - accumulatedAmount;
-
-if (accumulatedAmount > homeValue) {
-    console.log(`Можно купить дом`);
-    console.log(`На депозите осталось ${cashResidue}$`);
+// else if
+console.log('else if');
+if (role === 'manager') {
+    console.log('Менеджер');
+} else if (role === 'admin') {
+    console.log('Админ');
+} else if (role === 'ceo') {
+    console.log('CEO');
 } else {
-    console.log(`На дом нужно ещё накопить ${NotEnoughMoney}$`);
+    console.log('Мы тебя не знаем!');
+};
+
+// switch
+console.log('switch');
+switch (role) {
+    case 'manager':
+        console.log('Менеджер');
+        break
+    case 'admin':
+        console.log('Админ');
+        break
+    case 'ceo':
+        console.log('CEO');
+        break
+    default :
+        console.log('Мы тебя не знаем!');
 };
