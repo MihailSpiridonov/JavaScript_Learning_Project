73c5a59d9c3be986c7cd3551df83e4a41a5b8e88
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2022-12-20 13:51:29 +0300

    Выполнил четвёртую задачу интенсива

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 /*
-Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?". 
-Если ответ верен, выведите в консоли "Успех", если нет - "Вы робот!", 
-а если он введёт "Я не робот", то тоже "Успех".
+Пользователь хочет приобрести игру в магазине. Он может это сделать только если:
+Eго баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
+Он не забанен (isBanned)
+Игра не куплена (isExist)
+Игра в продаже (isSelling)
+Напишите условие для покупки и выведите в консоль результат.
 */ 
 
-const message = 'Сколько будет 7 + или - 15?';
-const result = prompt(message);
+let balanceMoney = 1000;
+let balancePoints = 100;
 
-switch (result) {
-    case '22':
-    case '-8':
-    case 'Я не робот':
-    case 'я не робот':
-        console.log('Успех');
-        break;
-    default :
-        console.log('Вы робот!');
+const needMoney = 1000;
+const needPoints = 100;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+if ((balanceMoney >= needMoney || balancePoints >= needPoints) && !isBanned && !isExist && isSelling) {
+    console.log('Да! Покупаю!');
+} else {
+    console.log('Нет! Купить не получится (((');
+    switch(true) {
+        case (balanceMoney < needMoney):
+            console.log('Не достаточно денег');
+        case (balancePoints < needPoints):
+            console.log('Не достаточно бонусов');
+            break;
+        case isBanned:
+            console.log('Вы забанены!');
+            break;
+        case isExist:
+            console.log('Игра уже куплена!');
+            break;
+        case !isSelling:
+            console.log('Игры нет в продаже!');
+            break;
+    };
 };
