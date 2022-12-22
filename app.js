6393c8ec143e03301fb7e17086604a8dfffccab8
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2022-12-22 11:21:35 +0300

    Выполнил пятую задачу интенсива - Виды функций

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
-/*
-Пользователь хочет приобрести игру в магазине. Он может это сделать только если:
-Eго баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
-Он не забанен (isBanned)
-Игра не куплена (isExist)
-Игра в продаже (isSelling)
-Напишите условие для покупки и выведите в консоль результат.
-*/ 
+/* 
+Задача:
+Переписать данную функцию в стрелочную  и положить её в переменную
+ */
 
-let balanceMoney = 1000;
-let balancePoints = 100;
+function toPower (num, power) {
+    const res = num ** power;
+    return res;
+}
 
-const needMoney = 1000;
-const needPoints = 100;
-const isBanned = false;
-const isExist = false;
-const isSelling = true;
+console.log(toPower(2, 3));
 
-if ((balanceMoney >= needMoney || balancePoints >= needPoints) && !isBanned && !isExist && isSelling) {
-    console.log('Да! Покупаю!');
-} else {
-    console.log('Нет! Купить не получится (((');
-    switch(true) {
-        case (balanceMoney < needMoney):
-            console.log('Не достаточно денег');
-        case (balancePoints < needPoints):
-            console.log('Не достаточно бонусов');
-            break;
-        case isBanned:
-            console.log('Вы забанены!');
-            break;
-        case isExist:
-            console.log('Игра уже куплена!');
-            break;
-        case !isSelling:
-            console.log('Игры нет в продаже!');
-            break;
-    };
-};
+/* Выполнение задачи */
+
+const toPowerArrow = (num, power) => num ** power;
+
+console.log(toPowerArrow(2, 3));
